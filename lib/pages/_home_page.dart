0fb498685d41b4c7ff202e862d1332f8f9117317
Facundo Tanovich <facundotanovich@gmail.com>
2021-08-23 08:33:33 -0300

    despues de cambiar home de stfl a stls

---
+++ lib/pages/_home_page.dart
@@ -0,0 +1,153 @@
+import 'dart:io';
+
+import 'package:animate_do/animate_do.dart';
+import 'package:ejemplo_app/data/dbase.dart';
+import 'package:ejemplo_app/models/plato_model.dart';
+import 'package:ejemplo_app/provider/data_provider.dart';
+import 'package:ejemplo_app/share_prefs/user_preferences.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final db = new Dbase();
+  @override
+  void initState() {
+    DataProvider.obtienePlatosProvider();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final pref = new UserPreferences();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Platos'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              pref.nombreUsuario = '';
+              Navigator.pushReplacementNamed(context, 'login');
+            },
+          )
+        ],
+      ),
+      body: StreamBuilder(
+          stream: DataProvider.streamPlatosController,
+          // initialData: initialData,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            }
+            final List<PlatoModel> lista = snapshot.data;
+            return ListView.separated(
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
+                itemCount: lista.length,
+                itemBuilder: (_, int index) {
+                  return Container(
+                    padding: EdgeInsets.all(10),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: _borradoDismiss(lista, index, context),
+                        )
+                      ],
+                    ),
+                  );
+                });
+          }),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            final PlatoModel lista2 =
+                new PlatoModel(codigo: '', descripcion: '', precio: 0);
+            Navigator.pushNamed(context, 'mantenimiento', arguments: lista2);
+          }),
+    );
+  }
+
+  Dismissible _borradoDismiss(
+      List<PlatoModel> lista, int index, BuildContext context) {
+    return Dismissible(
+      background: Container(
+        color: Colors.red,
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(
+                Icons.delete,
+                color: Colors.white,
+              ),
+              Text(
+                'Eliminar',
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      key: Key(lista[index].codigo),
+      onDismissed: (direction) {
+        db.borrarPlato(lista[index].codigo);
+        DataProvider.obtienePlatosProvider();
+      },
+      confirmDismiss: (_) {
+        return showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Eliminar plato'),
+            content: Text(
+              'Confirma la eliminacion?',
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text('No'),
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+              ),
+              TextButton(
+                child: Text('Si'),
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+      child: _lista(lista, index),
+    );
+  }
+
+  BounceInLeft _lista(List<PlatoModel> lista, int index) {
+    final item = lista[index];
+    print('Esto es ${item.picture.toString()}');
+    return BounceInLeft(
+      delay: Duration(milliseconds: 200 * index),
+      child: ListTile(
+        leading: item.picture != null
+            ? Image.file(File(item.picture.toString()))
+            : Image(image: AssetImage('images/no-image.png')),
+        title: Text(
+          item.descripcion,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        trailing: Text('\$ ' + item.precio.toString(),
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        onTap: () {
+          Navigator.pushNamed(context, 'mantenimiento', arguments: item);
+        },
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,19 +7,7 @@ import 'package:ejemplo_app/provider/data_provider.dart';
 import 'package:ejemplo_app/share_prefs/user_preferences.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final db = new Dbase();
-  @override
-  void initState() {
-    DataProvider.obtienePlatosProvider();
-    super.initState();
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final pref = new UserPreferences();
@@ -42,7 +30,7 @@ class _HomePageState extends State<HomePage> {
           // initialData: initialData,
           builder: (BuildContext context, AsyncSnapshot snapshot) {
             if (!snapshot.hasData) {
-              return Center(child: CircularProgressIndicator());
+              return Center(child: Container());
             }
             final List<PlatoModel> lista = snapshot.data;
             return ListView.separated(
@@ -71,83 +59,84 @@ class _HomePageState extends State<HomePage> {
           }),
     );
   }
+}
 
-  Dismissible _borradoDismiss(
-      List<PlatoModel> lista, int index, BuildContext context) {
-    return Dismissible(
-      background: Container(
-        color: Colors.red,
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Icon(
-                Icons.delete,
-                color: Colors.white,
-              ),
-              Text(
-                'Eliminar',
-                style: TextStyle(color: Colors.white),
-              ),
-            ],
-          ),
-        ),
-      ),
-      direction: DismissDirection.startToEnd,
-      key: Key(lista[index].codigo),
-      onDismissed: (direction) {
-        db.borrarPlato(lista[index].codigo);
-        DataProvider.obtienePlatosProvider();
-      },
-      confirmDismiss: (_) {
-        return showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: Text('Eliminar plato'),
-            content: Text(
-              'Confirma la eliminacion?',
+Dismissible _borradoDismiss(
+    List<PlatoModel> lista, int index, BuildContext context) {
+  return Dismissible(
+    background: Container(
+      color: Colors.red,
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Icon(
+              Icons.delete,
+              color: Colors.white,
             ),
-            actions: <Widget>[
-              TextButton(
-                child: Text('No'),
-                onPressed: () {
-                  Navigator.of(context).pop(false);
-                },
-              ),
-              TextButton(
-                child: Text('Si'),
-                onPressed: () {
-                  Navigator.of(context).pop(true);
-                },
-              ),
-            ],
-          ),
-        );
-      },
-      child: _lista(lista, index),
-    );
-  }
-
-  BounceInLeft _lista(List<PlatoModel> lista, int index) {
-    final item = lista[index];
-    print('Esto es ${item.picture.toString()}');
-    return BounceInLeft(
-      delay: Duration(milliseconds: 200 * index),
-      child: ListTile(
-        leading: item.picture != null
-            ? Image.file(File(item.picture.toString()))
-            : Image(image: AssetImage('images/no-image.png')),
-        title: Text(
-          item.descripcion,
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            Text(
+              'Eliminar',
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
         ),
-        trailing: Text('\$ ' + item.precio.toString(),
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-        onTap: () {
-          Navigator.pushNamed(context, 'mantenimiento', arguments: item);
-        },
       ),
-    );
-  }
+    ),
+    direction: DismissDirection.startToEnd,
+    key: Key(lista[index].codigo),
+    onDismissed: (direction) {
+      final db = new Dbase();
+      db.borrarPlato(lista[index].codigo);
+      DataProvider.obtienePlatosProvider();
+    },
+    confirmDismiss: (_) {
+      return showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Eliminar plato'),
+          content: Text(
+            'Confirma la eliminacion?',
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('No'),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+            ),
+            TextButton(
+              child: Text('Si'),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        ),
+      );
+    },
+    child: _lista(lista, index, context),
+  );
+}
+
+BounceInLeft _lista(List<PlatoModel> lista, int index, BuildContext context) {
+  final item = lista[index];
+  print('Esto es ${item.picture.toString()}');
+  return BounceInLeft(
+    delay: Duration(milliseconds: 200 * index),
+    child: ListTile(
+      leading: item.picture != null
+          ? Image.file(File(item.picture.toString()))
+          : Image(image: AssetImage('images/no-image.png')),
+      title: Text(
+        item.descripcion,
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+      trailing: Text('\$ ' + item.precio.toString(),
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+      onTap: () {
+        Navigator.pushNamed(context, 'mantenimiento', arguments: item);
+      },
+    ),
+  );
 }
